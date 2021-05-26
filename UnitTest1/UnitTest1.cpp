88b258ed50219fa5651ecb32375fdb1531e9d8a6
bilionok <photoshoper16@gmail.com>
2021-05-26 21:32:39 +0300

    la

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../9_1/9_1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
